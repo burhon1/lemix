ab50171b88a3d486ec2d6d1f48e59849c72a46b0
Burhon Baqoyev <burhonbaqoyev@gmail.com>
2022-06-23 16:00:26 +0500

    / urlga kirganda user/login/ ga otqazadigan bo'ldi

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='user/login/', permanent=False)),
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('education/', include('education.urls')),
