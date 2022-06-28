ef6dd162b69f6045dda2fe6271561b1be4b8f0d3
Azizbek <saidmurodovazizbek4@gmail.com>
2022-06-28 09:35:12 -0700

    kurs ichini

--- education/urls.py
+++ education/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('test/',test2_view,name='test'),
     path('groups/list/',groups_list_view,name='test'),
     path('courses/list/',courses_list_view,name='course'),
+    path('course/<int:id>/detail/',course_detail_view,name='course_detail'),
     
 ]
