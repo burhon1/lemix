223e1e41d90e32418e33cc5e8c2f3892a31db1d2
Azizbek <saidmurodovazizbek4@gmail.com>
2022-08-10 03:39:12 +0500

    159

--- education/urls.py
+++ education/urls.py
@@ -6,11 +6,12 @@ from education.views import *
 app_name = 'education'
 
 urlpatterns = [
+
+    
     path('test/',test2_view,name='test'),
     path('teachers/',teachers_view,name='teachers'),
     path('courses/list/',courses_list_view,name='courses-list'),
     path('course/<int:id>/detail/',course_detail_view,name='course_detail'),
-
     path('teacher/<int:id>/detail/',teacher_detail_view,name='teacher_detail'),
     path('employe/<int:id>/detail/',employe_detail_view,name='employe_detail'),
     path('group/<int:id>/detail/',group_detail_view,name='group_detail'),
@@ -18,7 +19,6 @@ urlpatterns = [
     path('parents/<int:id>/detail/',parents_detail_view,name='parents_detail'),
     path('debt/<int:id>/groups/',debt_groups_view,name='debt_groups'),
     path('debt/<int:id>/course/',debt_course_view,name='debt_course'),
-
     path('message/settings/',message_settings_view,name='message-settings'),
     path('teacher/add/',teacher_add_view,name='courseadd'),
     path('groups/list/',groupslist_view,name='groups-list'),
@@ -27,7 +27,6 @@ urlpatterns = [
     path('employees/list/',employees_view,name='employees-list'),
     path('debt/list/',debt_list_view,name='debt-list'),
     path('lid/first/',lid_first_view,name='lid-first'),
-    
     path('students/list/',students_list_view,name='students-list'),
     path('parents/list/',parents_list_view,name='parents-list'),
     path('expenses/list/',expenses_list_view,name='expenses-list'),
