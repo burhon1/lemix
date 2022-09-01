0efd1f85c385c5836ea3eb921d5060741224c693
Burhon <burhonbaqoyev@gmail.com>
2022-09-01 23:06:21 +0500

    ip add

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ sys.path.append(os.path.join(BASE_DIR, 'apps'))
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['5.8.248.139','t.lemix.uz','localhost', '127.0.0.1']
 DEFAULT_DOMAIN = 'https://{}'.format(ALLOWED_HOSTS[0])
-CSRF_TRUSTED_ORIGINS = ['http://*','https://*']
+CSRF_TRUSTED_ORIGINS = ['http://5.8.248.139','http://lemix.uz/','https://lemix.uz','https://*.lemix.uz','http://*.lemix.uz','http://lemix.uz','https://lemix.uz','http://*','https://*',]
 
 # Application definition
 
