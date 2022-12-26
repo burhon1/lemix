deea587aceb94475caecbd5bd0a565fcae194169
Burhon Baqoyev <burhon@192.168.1.5>
2022-12-27 01:05:15 +0500

    sda

--- core/settings.py
+++ core/settings.py
@@ -107,18 +107,18 @@ AUTH_USER_MODEL  = 'user.CustomUser'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'lemix',
-    #     'USER': 'postgres',
-    #     'PASSWORD': '1',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lemix',
+        'USER': 'djangouser',
+        'PASSWORD': '1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 
 }
 
