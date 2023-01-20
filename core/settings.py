822b8b2b7bece18d392460ce48ccac427f07df74
Burhon Baqoyev <burhon@192.168.1.4>
2023-01-20 06:27:36 +0500

    as

--- core/settings.py
+++ core/settings.py
@@ -5,10 +5,11 @@ import os, sys
 # from dotenv import  load_dotenv
 # load_dotenv('.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.getcwd())))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.getcwd())))
 # BASE_DIR = Path(__file__).resolve().parent.parent
 # TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # STATIC_DIRS = os.path.join(BASE_DIR, 'static')
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
