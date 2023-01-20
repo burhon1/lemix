5dfc1510738f81edd98592103b63f8e87819c01e
Burhon Baqoyev <burhon@192.168.1.4>
2023-01-20 06:32:03 +0500

    as

--- core/settings.py
+++ core/settings.py
@@ -171,8 +171,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [    
+#     BASE_DIR / 'static',
+#     ]
 # STATIC_ROOT = str(BASE_DIR.joinpath('static'))
 MEDIA_URL = '/media/uploads/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
