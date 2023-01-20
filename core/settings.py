1f6d690754cc05681e428488310d5756b0b1773d
burhon <burhonbaqoyev@gmail.com>
2023-01-20 18:51:35 +0500

    as

--- core/settings.py
+++ core/settings.py
@@ -171,8 +171,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/var/lemix/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/lemix/static/',
+]
 # STATIC_URL = 'static/'
 # STATICFILES_DIRS = [    
 #     BASE_DIR / 'static',
