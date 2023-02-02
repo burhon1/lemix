057d4228a0e64e5a453edaefd1b3590c735a6936
burhon <burhonbaqoyev@gmail.com>
2023-02-03 02:31:06 +0500

    sa

--- apps/admintion/admintion_processors.py
+++ apps/admintion/admintion_processors.py
@@ -4,15 +4,15 @@ def branch_list(request):
     if request.user.is_authenticated:
         ed_id=request.user.educenter
         educenter_ids = EduCenters.educenters.educenter_id_list(ed_id)  
-        branch = request.GET.get('branch',False)
+        # branch = request.GET.get('branch',False)
         
-        if branch:
-            request.session['branch_id']=branch
-        elif request.session.get('branch_id',False):
-            branch=request.session.get('branch_id',False)
-        else:
-            request.session['branch_id']=str(request.user.educenter)  
-            branch=request.user.educenter 
+        # if branch:
+        #     request.session['branch_id']=branch
+        # elif request.session.get('branch_id',False):
+        #     branch=request.session.get('branch_id',False)
+        # else:
+        #     request.session['branch_id']=str(request.user.educenter)  
+        #     branch=request.user.educenter 
         return {'branches':educenter_ids,'path_url':request.path,'selected_branch':0}
     return {}    
 
