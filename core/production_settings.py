3f60dde7281b9cf7784b2a1b8ef3c8fdb910e823
burhon <burhonbaqoyev@gmail.com>
2023-02-05 00:38:12 +0500

    add settings

--- core/production_settings.py
+++ core/production_settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lemix',
+        'NAME': 'lemixs',
         'USER': 'djangouser', #  djangouser  postgres
         'PASSWORD': '1',
         'HOST': '127.0.0.1',
