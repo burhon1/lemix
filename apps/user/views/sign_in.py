4a8ff7c8dd4e8dd759900b094176211361f2120f
burhon <burhonbaqoyev@gmail.com>
2023-02-05 02:24:19 +0500

    as

--- apps/user/views/sign_in.py
+++ apps/user/views/sign_in.py
@@ -25,7 +25,18 @@ def login_view(request):
                     next = request.GET.get('next')
                     if next:
                         return redirect(reverse('user:account')+f'?next={next}')
-                    return redirect(f'user:account')
+                    group = user.first().groups.values_list('name',flat=True)
+                    if len(group)!=0:
+                        next = request.GET.get('next', None)
+                        if next:
+                            return redirect(next)
+                        if 'Admintion' in group:
+                            return redirect('admintion:courses')
+                        elif 'Teacher' in group or 'Direktor' in group:
+                            return redirect('admintion:dashboard')
+                        elif 'Student' in group:
+                            return redirect('student:student')   
+                    return redirect('user:login')  
                 else:
                     context['error'] = "Parol xato kiritildi"   
             else:
