fc565505d46109548757f229a78f5571c26556e1
burhon <burhonbaqoyev@gmail.com>
2023-02-11 18:47:48 +0500

    sa

--- apps/admintion/views/dashboard.py
+++ apps/admintion/views/dashboard.py
@@ -5,19 +5,19 @@ from admintion.selectors import get_data_to_director,get_data_to_admin,get_data_
 
 def dashboard(request):
     context = dict()
-    group = request.user.groups.first()
+    group = request.user.groups.values_list('name',flat=True)
     
-    if group.name == 'Director':
+    if 'Director' in group:
         template_name = 'admintion/dashboard_director.html'
         context['is_director'] = True
         context.update(get_data_to_director(request.user))
 
-    if group.name == 'Admintion' or group.name == 'Manager':
+    if 'Admintion' in group or 'Manager' in group:
         template_name = 'admintion/dashboard_director.html'
         context['is_admin'] = True
         context.update(get_data_to_admin(request.user))
 
-    elif group.name == 'Teacher':
+    elif 'Teacher' in group:
         template_name = 'teachers/dashboard_teacher.html'
         context['is_teacher'] = True
         context.update(get_data_to_teacher(request.user))
