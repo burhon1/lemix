f4d6d6d8b516076a92a1fec919178e96ff96d338
burhon <burhonbaqoyev@gmail.com>
2023-03-06 00:45:50 +0500

    as

--- apps/admintion/querysets/lead_forms_managers.py
+++ apps/admintion/querysets/lead_forms_managers.py
@@ -1,14 +1,23 @@
-from django.db.models import Value, Case, When,F,Manager,Func,CharField,Q,TextField,Exists,OuterRef,Count,Sum
+from django.db.models import Value, Case, When,F,Manager,Func,FloatField,Q,CharField,Exists,OuterRef,Count,Sum
 from django.db.models.functions import Concat,Substr,Cast
 from django.contrib.postgres.aggregates import ArrayAgg
 from django.db.models.query import QuerySet
+from django.db.models import Func
+
+class Round2(Func):
+    function = 'ROUND'
+    template='%(function)s(%(expressions)s, 2)'
 
 class LeadFormQueryset(QuerySet):
     def lead_forms(self,educenter_id):
         return self.filter(Q(educenters__id__in=educenter_id)|Q(educenters__isnull=True)).annotate(
             send_count=Count('formlead', distinct=True)
         )\
-        .annotate(conversion=F('seen')/Value(100.0)*F('send_count'))\
+        .annotate(
+            conversion=Cast(Round2(Case(When(seen=0,then=Value(0)),
+                            default=(F('send_count')*1.0)/F('seen')*100,
+                            output_field=FloatField())), output_field=CharField())
+            )\
         .values('id','name','title','link','qrcode','seen','send_count','conversion').order_by('-id')
 
 class LeadFormManager(Manager):
